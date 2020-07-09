6efdec9c19dff815fbb7e77d97c6802a0aa57239
rishithSadashiv <rishith.1si16ec078@gmail.com>
2020-07-09 22:41:37 +0530

    added controller and repository

---
+++ Assignment2/Controllers/CustomerController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment2.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Assignment2.Entities;
+
+namespace Assignment2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerRepositories _repo = new CustomerRepositories();
+        // GET: CustomerController1
+        public ActionResult Index()
+        {
+            List<Customer> list = _repo.GetCustomers();
+            return View(list);
+        }
+
+        // GET: CustomerController1/Details/5
+        public ActionResult Details(int id)
+        {
+            Customer c = _repo.GetCustomerById(id);
+            return View(c);
+        }
+
+        // GET: CustomerController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Customer c = _repo.GetCustomerById(id);
+            return View(c);
+        }
+
+        // POST: CustomerController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customer c)
+        {
+            try
+            {
+                _repo.Update(c);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Customer c = _repo.GetCustomerById(id);
+            return View(c);
+        }
+
+        // POST: CustomerController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Customer c)
+        {
+            try
+            {
+                _repo.Delete(c);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Assignment2/Repositories/CustomerRepositories.cs
@@ -0,0 +1,60 @@
+﻿using Assignment2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+
+namespace Assignment2.Repositories
+{
+    public class CustomerRepositories
+    {
+        public List<Customer> GetCustomers()
+        {
+            using (CustomerContext db = new CustomerContext())
+            {
+                return db.Customer.ToList() ;
+                // return db.Employees.Where(i => i.Salary == 10000).ToList(); return multiple employees using salary
+            }
+
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            using (CustomerContext db  = new CustomerContext())
+            {
+                return db.Customer.Find(id);
+                //  return db.Employees.SingleOrDefault(i => i.Eid == eid); //search with any column
+            }
+
+        }
+
+        public void Add(Customer c)
+        {
+            using (CustomerContext db = new CustomerContext())
+            {
+                db.Customer.Add(c);
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(Customer c)
+        {
+            using (CustomerContext db = new CustomerContext())
+            {
+                db.Customer.Remove(c);
+                db.SaveChanges();
+            }
+
+        }
+
+        public void Update(Customer c)
+        {
+            using (CustomerContext db = new CustomerContext())
+            {
+                db.Customer.Update(c);
+                db.SaveChanges();
+            }
+        }
+    }
+}
